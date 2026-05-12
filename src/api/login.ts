20f50c4d1f389852f5db9194a97e7359afd1c778
Megumi0826 <211157888+Megumi0826@users.noreply.github.com>
2026-05-12 18:41:57 +0800

    chore: 初始化

---
+++ src/api/login.ts
@@ -0,0 +1,85 @@
+import type { IAuthLoginRes, ICaptcha, IDoubleTokenRes, IUpdatePassword, IUpdateProfile, IUserInfoRes } from './types/login'
+import { http } from '@/http/http'
+
+/**
+ * 登录表单
+ */
+export interface ILoginForm {
+  username: string
+  password: string
+}
+
+/**
+ * 获取验证码
+ * @returns ICaptcha 验证码
+ */
+export function getCode() {
+  return http.get<ICaptcha>('/user/getCode')
+}
+
+/**
+ * 用户登录
+ * @param loginForm 登录表单
+ */
+export function login(loginForm: ILoginForm) {
+  return http.post<IAuthLoginRes>('/auth/login', loginForm)
+}
+
+/**
+ * 刷新token
+ * @param refreshToken 刷新token
+ */
+export function refreshToken(refreshToken: string) {
+  return http.post<IDoubleTokenRes>('/member/auth/refresh-token', { refreshToken })
+}
+
+/**
+ * 获取用户信息
+ */
+export function getUserInfo() {
+  return http.get<IUserInfoRes>('/member/user/get')
+}
+
+/**
+ * 退出登录
+ */
+export function logout() {
+  return http.post<void>('/member/auth/logout')
+}
+
+/**
+ * 修改用户信息
+ */
+export function updateProfile(data: IUpdateProfile) {
+  return http.put<boolean>('/member/user/update-profile', data)
+}
+
+/**
+ * 修改用户密码
+ */
+export function updateUserPassword(data: IUpdatePassword) {
+  return http.post('/user/updatePassword', data)
+}
+
+/**
+ * 获取微信登录凭证
+ * @returns Promise 包含微信登录凭证(code)
+ */
+export function getWxCode() {
+  return new Promise<UniApp.LoginRes>((resolve, reject) => {
+    uni.login({
+      provider: 'weixin',
+      success: res => resolve(res),
+      fail: err => reject(new Error(err)),
+    })
+  })
+}
+
+/**
+ * 微信登录
+ * @param params 微信登录参数，包含code
+ * @returns Promise 包含登录结果
+ */
+export function wxLogin(data: { code: string }) {
+  return http.post<IAuthLoginRes>('/member/auth/weixin-mini-login', data)
+}
